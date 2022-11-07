4ef19aff4eea67d58a1a94dc3c17601e0712a8e0
AlexanderARodin <rodin_aa@inbox.ru>
2022-11-07 16:22:34 +0300

    add Swift SpritKit connector class

--- oldPhoneToSwift/ObjC/ObjcViewController.m
+++ oldPhoneToSwift/ObjC/ObjcViewController.m
@@ -5,15 +5,24 @@
 //  Created by the Dragon on 07.11.2022.
 //
 
+#import <SpriteKit/SpriteKit.h>
+
 #import "ObjcViewController.h"
+#import "oldPhoneToSwift-Swift.h"
 
 @implementation ObjcViewController
+
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	printf("viewDidLoad\n");
+	SKView *skView = (SKView *)self.view;
+	CGSize aSize = skView.bounds.size;
+	SwiftScene *swiftScene = [[SwiftScene alloc] initWithSize:aSize swapAxis:false];
+	[skView presentScene:swiftScene];
 }
+
+
 
 - (BOOL)shouldAutorotate {
 	return YES;
--- oldPhoneToSwift/SwiftConnector.swift
+++ oldPhoneToSwift/SwiftConnector.swift
@@ -5,4 +5,34 @@
 //  Created by the Dragon on 07.11.2022.
 //
 
-import Foundation
+import SpriteKit
+
+class SwiftScene: SKScene {
+	
+	override func update(_ currentTime: TimeInterval) {
+		//		print("update....")
+	}
+	
+	override func didMove(to view: SKView) {
+		view.showsFPS = true
+		view.showsNodeCount = true
+		print("REDRAW with a Size: \(size)")
+	}
+	
+	//	override
+	@objc
+	init(withSize: CGSize, swapAxis: Bool) {
+		if swapAxis {
+			super.init(size: CGSize(width: withSize.height, height: withSize.width))
+		}else{
+			super.init(size: withSize)
+		}
+		scaleMode = .aspectFit
+		anchorPoint.x = 0.5
+		anchorPoint.y = 0.5
+		print("CREATED with a Size: \(size)")
+	}
+	
+	required init?(coder aDecoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+
