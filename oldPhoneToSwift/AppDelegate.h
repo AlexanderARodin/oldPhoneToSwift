e1d6c44dd99771e3847bcee8e5de265584bef279
AlexanderARodin <rodin_aa@inbox.ru>
2022-11-07 14:38:54 +0300

    Initial Commit

---
+++ oldPhoneToSwift/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  oldPhoneToSwift
+//
+//  Created by the Dragon on 07.11.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oldPhoneToSwift/GameScene.h
@@ -0,0 +1,12 @@
+//
+//  GameScene.h
+//  oldPhoneToSwift
+//
+//  Created by the Dragon on 07.11.2022.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
